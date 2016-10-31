4537b11eb8a499f40dab99b05851e806754b500b
Caue Bruno de Almeida <RM30917@fiap.com.br>
2016-10-31 21:25:49 -0200

    Changing icons

--- app/src/main/java/fiap/com/menuapp/MainActivity.java
+++ app/src/main/java/fiap/com/menuapp/MainActivity.java
@@ -80,18 +80,10 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_fragments) {
+            // Handle the fragments action
+        } else if (id == R.id.nav_canvas) {
+            // Handle the canvas action
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
